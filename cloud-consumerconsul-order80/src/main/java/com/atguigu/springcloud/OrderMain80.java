8a5df550f3fb12958fc2875764a33e9540efa6a5
NutF4n <ccsdsg2016@gmail.com>
2022-12-29 16:44:43 +0800

    consul

---
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author cc
+ * @date 2022年12月29日 16:37
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderMain80 {
+     public static void main(String[] args) {
+           SpringApplication.run(OrderMain80.class, args);
+      }
+     
+}
